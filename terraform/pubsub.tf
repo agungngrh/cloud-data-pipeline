cb3e6b2c8ccfc2d3546d324b1f7efc50109e1e5b
AgungNugrahaa <anugrah171819@gmail.com>
2026-08-20 13:17:54 +0700

    feat(terraform): provision GCP infrastructure

--- terraform/pubsub.tf
+++ terraform/pubsub.tf
@@ -1,6 +1,19 @@
+resource "google_pubsub_schema" "trip_event" {
+  name    = "${var.resource_prefix}-trip-event-schema"
+  project = var.project_id
+  type    = "AVRO"
+
+  definition = file("${path.module}/schemas/trip_event.avsc")
+}
+
 resource "google_pubsub_topic" "trip_events" {
   name    = "${var.resource_prefix}-trip-events"
   project = var.project_id
+
+  schema_settings {
+    schema   = google_pubsub_schema.trip_event.id
+    encoding = "JSON"
+  }
 }
 
 resource "google_pubsub_subscription" "trip_events" {
