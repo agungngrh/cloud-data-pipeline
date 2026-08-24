d1fdf27293e7f69536d6779eda90f27cd8726585
AgungNugrahaa <anugrah171819@gmail.com>
2026-08-24 18:46:58 +0700

    refactor conflict dependency

--- tests/dags/test_dag_factory.py
+++ tests/dags/test_dag_factory.py
@@ -6,7 +6,7 @@ from airflow.dags.batch_pipeline import create_dbt_layer
 
 def test_create_dbt_layer_run_then_test_order():
     with DAG(dag_id="dummy_dag", start_date=datetime(2026, 1, 1), schedule=None):
-        group = create_dbt_layer(layer_name="staging", dbt_command="run", dbt_vars="{}")
+        group = create_dbt_layer(layer_name="staging", dbt_vars="{}")
 
         run_task = group.get_child_by_label("dbt_run_staging")
         test_task = group.get_child_by_label("dbt_test_staging")
